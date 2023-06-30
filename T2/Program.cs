79af39a39d9790fedc990116809ffd3b077e0fbc
eevgeniy <evlevgenij@yandex.ru>
2023-06-30 21:04:13 +0400

    начало решение задачи 3

--- T2/Program.cs
+++ T2/Program.cs
@@ -2,17 +2,17 @@
 // // [3, 7, 23, 12] -> 19
 // // [-4, -6, 89, 6] -> 0
 
-int[] array = GetArray(14,0,999);
-Console.WriteLine(String.Join(", ",array));
+int[] array = GetArray(14, 0, 999);
+Console.WriteLine(String.Join(", ", array));
 
 Console.WriteLine($"сумму элементов, стоящих на нечётных индексах равна {GetSum(array)}");
 
 int GetSum(int[] array)
 {
-    int sum=0;
+    int sum = 0;
     for (int i = 0; i < array.Length; i++)
     {
-        if (i % 2 !=0) sum += array[i];
+        if (i % 2 != 0) sum += array[i];
     }
     return sum;
 }
@@ -22,7 +22,7 @@ int[] GetArray(int size, int minValue, int maxValue)
     int[] result = new int[size];
     for (int i = 0; i < size; i++)
     {
-        result[i] = new Random().Next(minValue, maxValue +1);
+        result[i] = new Random().Next(minValue, maxValue + 1);
     }
     return result;
 }
